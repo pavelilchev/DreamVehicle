bc67bbb941f36779f74e2a135a1f5f474e3a9da4
Pavel Ilchev <pavel_ilchev@abv.bg>
2018-01-20 02:35:51 +0200

    added constants

---
+++ DreamVehicle.Web/Infrastructure/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace DreamVehicle.Web.Infrastructure
+{
+    public static class Constants
+    {
+        public const string RequiredValidationErrorMessage = "Моля попълнете {0}";
+
+        public const string RangeValidationErrorMessage = "Полето {0} трябва да бъде между {1} и {2}";
+
+        public const string StringLengthValidationErrorMessage = "Полето {0} трябва да бъде между {2} и {1} символа";
+
+        public const string ModelName = "Марка";
+
+        public const string ManufacturedYearName = "Година";
+
+        public const string HorsePowerName = "Мощност";
+
+        public const string ImporterName = "Вносител";
+
+        public const string DescriptionName = "Описание";
+    }
+}
---
+++ DreamVehicle.Web/Models/VehicleViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace DreamVehicle.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Infrastructure.Constants;
+
+    public class VehicleViewModel
+    {
+        [Required(ErrorMessage = RequiredValidationErrorMessage)]
+        [Display(Name = ModelName)]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = RequiredValidationErrorMessage)]
+        [Range(1800, 2100, ErrorMessage = RangeValidationErrorMessage)]
+        [Display(Name = ManufacturedYearName)]
+        public int ManufacturedYear { get; set; }
+
+        [Required(ErrorMessage = RequiredValidationErrorMessage)]
+        [Range(1, 1000, ErrorMessage = RangeValidationErrorMessage)]
+        [Display(Name = HorsePowerName)]
+        public int HorsePower { get; set; }
+
+        [Required(ErrorMessage = RequiredValidationErrorMessage)]
+        [Display(Name = ImporterName)]
+        public string Importer { get; set; }
+
+        [Required(ErrorMessage = RequiredValidationErrorMessage)]
+        [Display(Name = DescriptionName)]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = StringLengthValidationErrorMessage)]
+        public string Description { get; set; }
+    }
+}
